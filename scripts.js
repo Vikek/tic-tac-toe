0ccbb092a360ef4ac39fa653b304f79947559965
viktekbl <77540645+Vikek@users.noreply.github.com>
2023-09-18 19:28:00 +0200

    winning/losing logic and message

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 const Gameboard = (() => {
     let gameboard = new Array(9);
+    const winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-    const setValue = (index, value) => {
-        gameboard[index] = value;
+    const setMark = (index, mark) => {
+        gameboard[index] = mark;
 
         console.log(gameboard[index]);
         _render(index);
@@ -13,14 +23,22 @@ const Gameboard = (() => {
         boardSquares[index].classList.add(gameboard[index]);
     }
 
-    return { setValue }
+    const checkWin = (mark) => {
+        return winCombinations.some(combination => {
+            return combination.every(index => {
+                return gameboard[index] === mark;
+            })
+        })
+    }
+
+    return { setMark, checkWin }
 })();
 
-const Player = (type) => {
-    let _type = type;
-    const getType = () => _type;
+const Player = (mark) => {
+    let _mark = mark;
+    const getMark = () => _mark;
     
-    return { getType }
+    return { getMark }
 }
 
 const Game = (() => {
@@ -38,10 +56,25 @@ const Game = (() => {
         const squareIndex = Array.from(boardSquares).indexOf(e.target);
 
         if(playerTurn) {
-            Gameboard.setValue(squareIndex, player.getType());
-        }
-        else {
-            Gameboard.setValue(squareIndex, computer.getType());
+            let playerMark = player.getMark();
+            Gameboard.setMark(squareIndex, playerMark);
+            if(Gameboard.checkWin(playerMark)) { // player win
+                let messageText = document.querySelector(".game-end-message-text");
+                messageText.textContent = "You win!"
+
+                let message = document.querySelector(".game-end-message");
+                message.classList.toggle("active");
+            }
+        } else {
+            let computerMark = computer.getMark();
+            Gameboard.setMark(squareIndex, computerMark);
+            if(Gameboard.checkWin(computerMark)) { // computer win
+                let messageText = document.querySelector(".game-end-message-text");
+                messageText.textContent = "You lose!"
+
+                let message = document.querySelector(".game-end-message");
+                message.classList.toggle("active");
+            }
         }
 
         playerTurn = !playerTurn;
